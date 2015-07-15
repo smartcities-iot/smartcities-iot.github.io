80f258ef608028d54b3495f55dcba7f92040a142
Jenny Wen <jennywen27@gmail.com>
2015-07-15 14:12:25 -0400

    styling a bunch of shit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var client = new Keen({
     projectId: "55907f6bd2eaaa236e17a521",
-    readKey: "e21923dd742fe25ccdfe441827a502fb1db71888ad0444d11bfab92459aa1e103cdf1574f982de3f1da67191e7d5b397b7926791b8af60a2b59f964557b7d10dc8980d85abcbbe60e03008eb306557d8ac2297fb62d1362db0269b8854acacb6da744b5c4970c783a26db17ec28c58ca"
+    readKey: "e21923dd742fe25ccdfe441827a502fb1db71888ad0444d11bfab92459aa1e103cdf1574f982de3f1da67191e7d5b397b7926791b8af60a2b59f964557b7d10dc8980d85abcbbe60e03008eb306557d8ac2297fb62d1362db0269b8854acacb6da744b5c4970c783a26db17ec28c58ca",
+    writeKey: "632fb8ad6f50f434a8d7d04b63a55da291a971e3b260df04cd4cb3809346870da9729f1ea7f8c40007cb96a726869808efff15ab174e982de77d1c70d55861142e2ee8a9efafb7f0b3010d96ba93bf23cc9eee5e794ebe233cca625fa95e244e6beba72ba1cd2b9d6686e83adc384da7"
 });
 
 var stopCount = new Keen.Query("count_unique", {
@@ -43,24 +44,38 @@ client.run(passengerCount, function(err, response){
   $('#numCars').html(Math.floor(response.result[0].result/1.5));
 });
 
-client.draw(count, document.getElementById("chart"), {
-  chartType: "areachart",
-    title: false,
-    height: 250,
-    width: "auto",
-    chartOptions: {
-      chartArea: {
-        height: "75%",
-        left: "10%",
-        top: "5%",
-        width: "60%"
-      },
-      isStacked: true
-    }
+// client.draw(count, document.getElementById("chart"), {
+//   chartType: "areachart",
+//     title: false,
+//     height: 250,
+//     width: "auto",
+//     chartOptions: {
+//       chartArea: {
+//         height: "75%",
+//         left: "10%",
+//         top: "5%",
+//         width: "60%"
+//       },
+//       isStacked: true
+//     }
+// });
+
+
+// Click events
+$('.change-stop-action-button').on('click', function() {
+  $('.change-route-container').show();
 });
 
-// client.run(count, function(err, response){
-//   // if (err) handle the error
-//   $("body").html(response.result);
-//   console.log(response.result); // 100
-// });
+$('.map-card-request-button').on('click', function() {
+  var stopRequest = {
+    card: '1',
+    stopnum: '213123',
+    keen: {
+      timestamp: new Date().toISOString()
+    }
+  }
+  console.log(stopRequest);
+  $('.map-card-request-button').html('Stop requested').attr('disabled', 'disabled');
+  // Sends to "stopRequest" collection
+  client.addEvent("stopRequest", stopRequest);
+});
